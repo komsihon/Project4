ac1722138d2bf31171132ed2a407f316f89305a4
Kom Sihon <rsihon@gmail.com>
2017-12-18 18:30:11 +0100

    About URL removed

--- webnode/urls.py
+++ webnode/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from ikwen_webnode.webnode.views import AboutUs,  Home,\
-    ItemList, Portfolio, ProductDetails, DeployCloud
+from ikwen_webnode.webnode.views import Home, ItemList, Portfolio, ProductDetails, DeployCloud
 
 from django.conf import settings
 
@@ -18,7 +17,6 @@ else:
         '',
         url(r'^$', Home.as_view(), name='home'),
         url(r'^portfolio$', Portfolio.as_view(), name='portfolio'),
-        url(r'^about$', AboutUs.as_view(), name='about'),
         url(r'^item_list/(?P<slug>[-\w]+)/$', ItemList.as_view(), name='item_list'),
         url(r'^item/(?P<slug>[-\w]+)/$', ProductDetails.as_view(), name='product_details'),
     
