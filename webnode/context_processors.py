0fb6e6db84df67c6073f77bb3501acb266662e34
Kom Sihon <rsihon@gmail.com>
2018-01-11 18:16:19 +0100

    get_menu_list() replaced by menu_list

--- webnode/context_processors.py
+++ webnode/context_processors.py
@@ -1,5 +1,4 @@
-from django.conf import settings
-from ikwen_kakocase.commarketing.models import SmartCategory
+from ikwen_webnode.commarketing.models import SmartCategory
 from ikwen_kakocase.kakocase.context_processors import project_settings as kakocase_settings
 
 
@@ -7,23 +6,10 @@ def project_settings(request):
     """
     Adds utility project url and ikwen base url context variable to the context.
     """
-    webnode_settings = kakocase_settings(request)['settings']
-    webnode_settings.update({
-        'IS_WEBNODE': True,
-    })
-    return {
-        'settings': webnode_settings
-    }
+    webnode_settings = kakocase_settings(request)
+    webnode_settings['settings']['IS_WEBNODE'] = True
+    return webnode_settings
 
 
-def get_menu_list(request):
-    Home_link_id = getattr(settings, 'HOME_ID', None)
-    webnode_menu = SmartCategory.objects.filter(is_active=True).exclude(pk=Home_link_id).order_by('title')
-    menu_list = []
-    for menu in webnode_menu:
-        nav = {
-            'menu': menu,
-            'category_list': menu.get_category_queryset()
-        }
-        menu_list.append(nav)
-    return {'smart_cat_list': menu_list}
+def menu_list(request):
+    return {'menu_list': SmartCategory.objects.filter(is_active=True).order_by('order_of_appearance')}
